d936466d37caa503ae5eb0d0a420f3f7c55d41f4
Michael Haeuslmann <haeuslmann@gmail.com>
2013-10-21 21:50:43 +0700

    Moved functionality from the procedural script to the SubCollector class.

--- bin/sub-collector.php
+++ bin/sub-collector.php
@@ -2,4 +2,22 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-new Mihaeu\SubCollector();
+// script needs to handle huge files (e.g. bluray rips with 16GB)
+ini_set('memory_limit', -1);
+
+// Go ...
+echo '----------------------------------------'.PHP_EOL.
+    'Looking for movies without subtitles ...'.PHP_EOL.
+    '----------------------------------------'.PHP_EOL.PHP_EOL;
+
+$subCollector = new Mihaeu\SubCollector();
+$movies = $subCollector->findMoviesInFolder('/media/media/videos/movies');
+foreach ($movies as $movie)
+{
+    if ($subCollector->movieHasNoSubtitle($movie))
+    {
+        $subCollector->addSubtitleToMovie($movie);
+    }
+}
+
+
